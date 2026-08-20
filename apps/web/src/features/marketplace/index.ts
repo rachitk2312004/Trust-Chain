1493cb8b067577cdba3f2c5722b3ca5f895b2823
Rachit Kumar <rachitkumar2312004@gmail.com>
2026-08-20 06:44:18 +0530

    phase 1

---
+++ apps/web/src/features/marketplace/index.ts
@@ -0,0 +1,13 @@
+export {
+  marketplaceKeys,
+  useMarketplace,
+  useMarketplaceReviews,
+  useMarketplaceAnalytics,
+  usePublishConnector,
+  usePatchMarketplaceConnector,
+  useInstallMarketplaceConnector,
+} from "./hooks";
+export { MarketplaceTable } from "./MarketplaceTable";
+export { ConnectorDetailCard } from "./ConnectorDetailCard";
+export { InstallationDialog } from "./InstallationDialog";
+export { MarketplaceAnalyticsCard } from "./MarketplaceAnalyticsCard";
